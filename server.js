3ca5876170ef1b29fa1d1e0cb5c0b428a342d410
Teddylacassin <teddylacassin@hotmail.com>
2026-09-02 23:45:55 +0200

    Add appointment reminder scheduler to server

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ dns.setDefaultResultOrder('ipv4first');
 const { initDb } = require('./db');
 const { startReminderScheduler } = require('./reminders');
 const { startBankSyncScheduler } = require('./bankSync');
+const { startAppointmentReminderScheduler } = require('./appointmentReminders');
 
 const clientRoutes = require('./routes/client');
 const adminRoutes = require('./routes/admin');
@@ -40,6 +41,7 @@ initDb()
     });
     startReminderScheduler();
     startBankSyncScheduler();
+    startAppointmentReminderScheduler();
   })
   .catch((err) => {
     console.error('Erreur lors de l\'initialisation de la base de données :', err);
