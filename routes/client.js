657510f8a7d64b988f5dc91c376e1d1dfb9a64f3
Teddylacassin <teddylacassin@hotmail.com>
2026-09-12 17:32:57 +0200

    Update Cache-Control and add response headers

--- routes/client.js
+++ routes/client.js
@@ -158,9 +158,12 @@ router.get('/points-stream', (req, res) => {
 
   res.writeHead(200, {
     'Content-Type': 'text/event-stream',
-    'Cache-Control': 'no-cache',
+    'Cache-Control': 'no-cache, no-transform',
     'Connection': 'keep-alive',
+    'X-Accel-Buffering': 'no',
+    'Content-Encoding': 'identity',
   });
+  res.flushHeaders();
   res.write('\n');
 
   const listener = (data) => {
